ef8fa3c4feb5cc4c04cdb365c31c2f057b1d64e5
Tony Kumin <tony@kumin.ch>
2017-03-05 20:37:58 +0100

    nodejs skelleton

---
+++ server.js
@@ -0,0 +1,8 @@
+
+
+var port = 3000;
+var express = require("express");
+var app = express();
+var server = app.listen(port);
+app.use(express.static('public')); //makes the folder "public" be used in express
+console.log("connected via port "+port);
