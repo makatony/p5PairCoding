69770c6c0364e8fa25b6b854d74b54ba50beb58e
Tony Kumin <tony@kumin.ch>
2017-03-19 20:17:59 +0100

    flow

--- public/2dPerlinflowfield/sketch.js
+++ public/2dPerlinflowfield/sketch.js
@@ -1,6 +1,9 @@
 var CELLWITDH = 10;
 var inc = 0.1;
 var cells = [];
+var zoff = 0;
+var xoff = 0;
+var yoff = 0;
 
 function preload() {}
 
@@ -20,23 +23,23 @@ function setup() {
 
 function draw() {
 	background(255);
-	var xoff = 0;
-	var yoff = 0;
-	noLoop();
-	//newest perlin value
+	xoff = 0;
+	yoff = 0;
+	// noLoop();
 	
 	
 	for (var x = 0; x < COLS; x++) {
 		xoff = 0;
 		for (var y = 0; y < ROWS; y++) {
 			var cell = cells[index(x, y)];
-			var perlin = noise(xoff,yoff) * TWO_PI;
+			var perlin = noise(xoff, yoff, zoff) * TWO_PI;
 			cell.vector = p5.Vector.fromAngle(perlin);
 			xoff += inc;
 			
 		}
 		yoff += inc;
 	}
+	zoff += inc / 4; //affected by time -> cannot be set to zero in draw function
 	
 	for (i in cells) {
 		var cell = cells[i];
